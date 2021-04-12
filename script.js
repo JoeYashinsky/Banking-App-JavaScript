6bcecafe71ed318f2ed2f79070058402fec61cce
Your Name <joey.yashinsky@gmail.com>
2021-04-12 15:49:48 -0400

    correct typo where where single underscore was supposed to be 2 consec. underscore characters

--- script.js
+++ script.js
@@ -62,20 +62,20 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
-    <div class="movements_row">
-      <div class="movements_type movements_type--${type}">${
-        i + 1
-    } ${type}</div>
-      <div class="movements_value">${mov}</div>
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}
+        </div>
+        <div class="movements__value">${mov}</div>
       </div>
-    `;
+      `;
 
-    containerMovements.insertAdjacentHTML
-    ('afterbegin')
+      containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements);
