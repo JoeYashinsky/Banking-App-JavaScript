016666a80347edcce2ce0d2edddb5f9e1f724491
Your Name <joey.yashinsky@gmail.com>
2021-04-15 15:10:08 -0400

    map method utilized to convert each euro value to correct dollar value ( multiplied by 1.1)

--- script.js
+++ script.js
@@ -216,3 +216,7 @@ currenciesUnique.forEach(function (value, _, map) {
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUSD = 1.1;
+
+const movementsUSD = movements.map(function(mov) {
+  return mov * eurToUSD;
+})
